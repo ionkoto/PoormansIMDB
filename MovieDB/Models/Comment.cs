3ae0fd2c802174a51cd021915eb19cc6cb748e8b
mihail-kovashki <mihail.kovashki@yahoo.com>
2016-08-30 21:07:51 +0300

    Comments Functionality

---
+++ MovieDB/Models/Comment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieDB.Models
+{
+    public class Comment
+    {
+        public Comment()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        public int CommentId { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string Author_Id { get; set; }
+
+        [ForeignKey("Author_Id")]
+        public virtual ApplicationUser Author { get; set; }
+
+        public int MovieId { get; set; }
+
+        [ForeignKey("MovieId")]
+        public virtual Movie Movie { get; set; }
+    }
+}
